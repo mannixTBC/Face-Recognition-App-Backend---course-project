6f2150271f3d14ddfd454896e7e781a083c2161e
teo <teofilbadelita@gmail.com>
2020-04-04 17:27:42 +0300

    .

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const register = require('./controllers/register')
 const db = knex({
     client: 'pg',
     connection: {
-      connectionString : DATABASE_URL,
+      connectionString : 'postgres://rgbrdgfnnuippj:382a9f03ad80b9bce5582b7840aef184945aacc463fb531872a34ea9423342c2@ec2-54-159-112-44.compute-1.amazonaws.com:5432/damkr31mu4k49j ',
       ssl:true  
     }
   });
