5f58379f1a3c7c7ff6cd85e3bdbd58a6ff8ee11a
teo <teofilbadelita@gmail.com>
2020-04-04 17:21:45 +0300

    .

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const register = require('./controllers/register')
 const db = knex({
     client: 'pg',
     connection: {
-      connectionString : process.env.DFATABASE_URL,
+      connectionString : DATABASE_URL,
       ssl:true  
     }
   });
