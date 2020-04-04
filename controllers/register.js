c81e91645db5f6e21f63d8c6d4b2bfb372e00904
teo <teofilbadelita@gmail.com>
2020-04-04 16:53:15 +0300

    .

--- controllers/register.js
+++ controllers/register.js
@@ -3,26 +3,26 @@
 
 
 const handleRegister = (req,res,db,bcrypt) => {
-    const {email,name,pasword} = req.body
-    if(!email || !name || !pasword){
+    const {email,name,password} = req.body
+    if(!email || !name || !password){
        return res.status(400).json('incorect form submision')
     }
-    const hash = bcrypt.hashSync(pasword)
+    const hash = bcrypt.hashSync(password)
     
     db.transaction(trx=> {
         trx.insert({
             hash:hash,
             email:email
-        }).catch(err => res.send(json('can t insert1')))
+        }).catch(err => res.send(json('can t insert')))
         .into('login')
-        .returning('email').catch(err => res.send(json('can t insert2')))
+        .returning('email')
         .then(loginEmail => {
             console.log(loginEmail)
             return trx('users').returning('*').insert({
                 email:loginEmail[0],
                 name:name,
                 joined: new Date()
-            }).catch(err => res.send(json('can t insert3'))).then(user => {
+            }).then(user => {
                 res.json(user[0])
             })
         }).then(trx.commit)
