32bbce2260a42292a341e878ae8f1cc3f0143a3d
teo <teofilbadelita@gmail.com>
2020-04-06 16:58:47 +0300

    controllers update

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const app = express()
 const knex = require('knex')
 const register = require('./controllers/register')
 const signin = require('./controllers/signin')
-
-
+const updateProfile = require('./controllers/updateProfile')
+const updateEntries = require('./controllers/updateEntries')
 
 
 const db = knex({
@@ -61,35 +61,9 @@ app.post('/signin', (req,res) => {signin.handleSignIn(req,res,db,bcrypt)})
 
 app.post('/register', (req,res) => {register.handleRegister(req,res,db,bcrypt)})
 
-app.get("/profile/:id", (req,res) => {
-    const {id} = req.params;
-   
-    db.select('*').from('users').where({
-        id:id
-    }).then(user => {
-        if(user.length){
-            res.json(user[0])
-        }else{
-            res.status(400).json('not found')
-        }
-        
-    }).catch(err => res.status(400).json('error geting user'))
-    
-} )
+app.get("/profile/:id", (req,res) => {updateProfile.updateProfile(req,res,db)} )
 
-app.put('/image', (req,res) => {
-    const {id} = req.body;
-    db('users').where('id', '=', id)
-    .increment('entries',1)
-    .returning('entries' )
-    .then(entries => {
-        res.send(entries[0])
-    }).catch(err => res.status(400).json('can t update entries'))
-})
-
-
-
-
+app.put('/image', (req,res) => {updateEntries.handleEntries(req,res)})
 
 
 app.listen(process.env.PORT || 3000, () => {
